1d38480c3f1b7a4a142183f4a148f48762fe64b9
Bohdan Rudenko <brudenko2804@gmail.com>
2024-06-26 21:40:02 +0300

    Complete the task

---
+++ src/main/java/com/epam/mjc/StudentNotFoundException.java
@@ -0,0 +1,10 @@
+package com.epam.mjc;
+
+public class StudentNotFoundException extends IllegalArgumentException {
+    public StudentNotFoundException(String s) {
+        super(s);
+    }
+
+    public StudentNotFoundException() {
+    }
+}
